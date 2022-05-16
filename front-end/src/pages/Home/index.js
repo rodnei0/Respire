2eebef985c6fb05ba8a37da80539b428a995bff5
rodnei0 <rodnei0@gmail.com>
2022-05-16 18:34:28 -0400

    test

--- front-end/src/pages/Home/index.js
+++ front-end/src/pages/Home/index.js
@@ -1,5 +1,31 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
+import WidgetsIcon from '@mui/icons-material/Widgets';
+
 export default function Home() {
     return (
-        <div>teste</div>
+        <>
+            <CssBaseline />
+            <AppBar position='relative'>
+                <Typography variant='h6'>
+                    Respire
+                </Typography>
+                {/* <Toolbar>
+                    <WidgetsIcon />
+                </Toolbar> */}
+            </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth='sm' bgco>
+                        <Typography variant='h2'>
+                            Respire
+                        </Typography>
+                    </Container>
+                </div>
+            </main>
+        </>
+
     );
 }
