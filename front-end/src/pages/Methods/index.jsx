d1aa88583596fbc7a2b79e5eea400269885fcc71
rodnei0 <rodnei0@gmail.com>
2022-05-23 17:00:58 -0400

    bug fixes

--- front-end/src/pages/Methods/index.jsx
+++ front-end/src/pages/Methods/index.jsx
@@ -43,7 +43,6 @@ export default function Methods() {
 	const [step, setStep] = React.useState("");
 	const [stepTime, setStepTime] = React.useState(breatheInTime);
 	const navigate = useNavigate();
-	console.log(cycle);
 
 	React.useEffect(() => {
 		BreathAnimation(breatheInTime, holdTime, breatheOutTime, method, setAnimation, setStep, setStepTime);
