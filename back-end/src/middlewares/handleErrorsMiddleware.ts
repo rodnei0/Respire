1bd06124431b45499f0c95e18ce65001809c2106
rodnei0 <rodnei0@gmail.com>
2022-05-23 12:19:04 -0400

    bug fixes

--- back-end/src/middlewares/handleErrorsMiddleware.ts
+++ back-end/src/middlewares/handleErrorsMiddleware.ts
@@ -44,7 +44,9 @@ import { AppError, isAppError, serviceErrorToStatusCode } from "../utils/errorUt
 //   };
 // }
 
+
 export function handleErrorsMiddleware(err: Error | AppError, req: Request, res: Response, next: NextFunction) {
+  console.log(err)
   if (isAppError(err)) {
     return res.status(serviceErrorToStatusCode[err.type]).send(err.message);
   }
